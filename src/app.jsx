734284b2413cba8dbddc1e18bad94c76f48ecc7b
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-02 17:36:19 -0500

    added a reusable button

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,44 @@
 var Button = React.createClass({
+    handleClick: function() {
+        this.props.handleClick(this.props.increment);
+    },
     render: function() {
         return (
-            <button>Go</button>
+            <button onClick={this.handleClick}>Add {this.props.increment}</button>
         )
     }
 });
 
-ReactDOM.render(<Button />, document.getElementById('root'));
+var Result = React.createClass({
+    render: function() {
+        return (
+            <div>{this.props.counterValue}</div>
+        );
+    }
+});
+
+var Main = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+    handleClick: function(increment) {
+        this.setState({
+            counter: this.state.counter + increment
+        });
+    },
+    render: function() {
+        return (
+            <div>
+                <Button handleClick={this.handleClick} increment={1} />
+                <Button handleClick={this.handleClick} increment={5} />
+                <Button handleClick={this.handleClick} increment={10} />
+                <Button handleClick={this.handleClick} increment={100} />
+                <Result counterValue={this.state.counter} />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Main />, document.getElementById('root'));
