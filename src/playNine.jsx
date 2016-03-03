232572487bb500f5314e74bdeafa78a88cd68372
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-03 16:49:07 -0500

    filled out all the basic UI features

--- src/playNine.jsx
+++ src/playNine.jsx
@@ -1,9 +1,7 @@
 var StarsFrame = React.createClass({
     render: function() {
-        var numberOfStars = Math.floor(Math.random() * 9) + 1;
-
         var stars = [];
-        for (var i = 0; i < numberOfStars; i++) {
+        for (var i = 0; i < this.props.numberOfStars; i++) {
             stars.push(
                 <span className="glyphicon glyphicon-star"></span>
             );
@@ -21,9 +19,10 @@ var StarsFrame = React.createClass({
 
 var ButtonFrame = React.createClass({
     render: function() {
+        var disabled = (this.props.selectedNumbers.length === 0);
         return (
             <div id="button-frame">
-                <button className="btn btn-primary btn-lg">=</button>
+                <button className="btn btn-primary btn-lg" disabled={disabled}>=</button>
             </div>
         );
     }
@@ -31,9 +30,16 @@ var ButtonFrame = React.createClass({
 
 var AnswerFrame = React.createClass({
     render: function() {
+        var props = this.props;
+        var selectedNumbers = props.selectedNumbers.map(function(i) {
+            return (
+                <span onClick={props.unselectNumber.bind(null, i)}>{i}</span>
+            );
+        });
         return (
             <div id="answer-frame">
                 <div className="well">
+                    {selectedNumbers}
                 </div>
             </div>
         );
@@ -44,10 +50,12 @@ var NumbersFrame = React.createClass({
     render: function() {
         var numbers = 9;
 
-        var numberBadges = [];
+        var numberBadges = [], className;
+        var selectedNumbers = this.props.selectedNumbers;
         for (var i = 1; i <= numbers; i++) {
+            className = 'number selected-' + (selectedNumbers.indexOf(i) >= 0);
             numberBadges.push(
-                <div className="number">{i}</div>
+                <div className={className} onClick={this.props.selectNumber.bind(null, i)}>{i}</div>
             );
         }
 
@@ -62,15 +70,39 @@ var NumbersFrame = React.createClass({
 });
 
 var PlayNineGame = React.createClass({
+    getInitialState: function() {
+        return {
+            selectedNumbers: [],
+            numberOfStars: Math.floor(Math.random() * 9) + 1
+        }
+    },
+    selectNumber: function(clickedNumber) {
+        if (this.state.selectedNumbers.indexOf(clickedNumber) < 0) {
+            this.setState({
+                selectedNumbers: this.state.selectedNumbers.concat(clickedNumber)
+            });
+        }
+    },
+    unselectNumber: function(clickedNumber) {
+        var selectedNumbers = this.state.selectedNumbers;
+        var indexOfNumber = selectedNumbers.indexOf(clickedNumber);
+        selectedNumbers.splice(indexOfNumber, 1);
+
+        this.setState({
+            selectedNumbers: selectedNumbers
+        });
+    },
     render: function() {
+        var selectedNumbers = this.state.selectedNumbers;
+        var numberOfStars = this.state.numberOfStars;
         return (
             <div>
                 <div className="clearfix">
-                    <StarsFrame />
-                    <ButtonFrame />
-                    <AnswerFrame />
+                    <StarsFrame numberOfStars={numberOfStars} />
+                    <ButtonFrame selectedNumbers={selectedNumbers} />
+                    <AnswerFrame selectedNumbers={selectedNumbers} unselectNumber={this.unselectNumber} />
                 </div>
-                <NumbersFrame />
+                <NumbersFrame selectedNumbers={selectedNumbers} selectNumber={this.selectNumber} />
             </div>
         );
     }
